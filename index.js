b342a999e00888b2beb067a4351ec030b44da369
oriakita <35460456+oriakita@users.noreply.github.com>
2018-11-26 00:56:18 +0700

    26/11

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket){
+    console.log('a user connected: ' + socket);
+    socket.broadcast.emit('hi');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(4000, function(){
+    console.log('listening on *:4000');
+});
